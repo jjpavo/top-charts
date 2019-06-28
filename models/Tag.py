92fc3171c2cfb6c97ac5bcb370014354ae6893c7
jjpavo <jjpavo@my.uri.edu>
2019-06-28 11:40:13 -0400

    Update Tag model
    Changed plural to singular since the model represents one tag. Added 'unique' constraint since there shouldn't be duplicates. Added a default value because Django was complaining about needing one for a non-null field. I chose to not use the null constraint due to the documentation suggesting to generally avoid doing so for CharFields https://docs.djangoproject.com/en/2.2/ref/models/fields/#null

--- models/Tag.py
+++ models/Tag.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    tags = models.CharField(max_length=250)
+    tag = models.CharField(max_length=250, unique=True, default="")
 
     def __str__(self):
-        return self.tags
+        return self.tag
