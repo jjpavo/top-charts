db65726b51dc917b36e63d29a305514b3dc8979b
jjpavo <jjpavo@my.uri.edu>
2019-07-10 19:20:26 -0400

    Allow chart to have NULL user
    This allows charts to be public.

--- models/Chart.py
+++ models/Chart.py
@@ -9,7 +9,7 @@ class Chart(models.Model):
     name = models.CharField(max_length=250)
     creation_date = models.DateField('chart created on')
     modification_date = models.DateField('chart last modified on')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.user.username}: {self.name}"
